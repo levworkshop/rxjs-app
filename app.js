7b634b5f9c77ab6776517430b09d26db5a67f977
levworkshop <noreply@githubusers.com>
2023-02-16 19:52:01 +0200

    http

--- app.js
+++ app.js
@@ -78,10 +78,76 @@
 // result.subscribe(x => console.log(x))
 
 
-const div = document.createElement('div');
-div.style.cssText = 'width: 200px; height: 200px; background: #09c;';
-document.body.appendChild(div);
+// const div = document.createElement('div');
+// div.style.cssText = 'width: 200px; height: 200px; background: #09c;';
+// document.body.appendChild(div);
 
-const clicks = rxjs.fromEvent(document, 'click');
-const clicksOnDivs = clicks.pipe(rxjs.filter(ev => (ev.target).tagName === 'DIV'));
-clicksOnDivs.subscribe(x => console.log(x));
+// const clicks = rxjs.fromEvent(document, 'click');
+// const clicksOnDivs = clicks.pipe(rxjs.filter(ev => (ev.target).tagName === 'DIV'));
+// clicksOnDivs.subscribe(x => console.log(x));
+
+
+// function doStuff() {
+//     return new Promise((resolve, reject) => {
+//         resolve('success');
+//         reject('error occured');
+//     });
+// }
+
+// async function tryIt() {
+//     try {
+//         const msg = await doStuff();
+//         console.log(msg);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+
+
+// function doStuff2() {
+//     return new rxjs.Observable(myStream => {
+//         myStream.next('success');
+//         myStream.complete();
+//     });
+// }
+
+// function tryIt2() {
+//     doStuff2().subscribe({
+//         next(msg) { console.log(msg) },
+//         error(err) { console.error(err) },
+//         complete() { console.log('done') },
+//     });
+// }
+
+
+// const myDatabase = new MyDatabase();
+// rxjs.Observable
+//     .defer(() => myDatabase.connect()) // returns a promise, promise do something one time...
+//     .retry(3)
+//     .subscribe();
+
+
+
+// rxjs.ajax
+//     .ajax('https://api.github.com/users?per_page=5')
+//     .subscribe(
+//         res => console.log(res),
+//         err => console.error(err)
+//     );
+
+
+
+const obs$ = ajax.getJSON('https://api.github.com/users?per_page=5')
+    .pipe(
+        rxjs.map(userResponse => console.log('users: ', userResponse)),
+        rxjs.catchError(error => {
+            console.log('error: ', error);
+            return rxjs.of(error);
+        })
+    );
+
+obs$.subscribe({
+    next: value => console.log(value),
+    error: err => console.log(err)
+});
